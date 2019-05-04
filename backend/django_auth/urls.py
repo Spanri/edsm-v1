77e599414701889ea22a768bb6df5bf5baa1f141
Spanri <kozlova9v@mail.ru>
2019-05-04 22:01:29 +0300

    auth

--- backend/django_auth/urls.py
+++ backend/django_auth/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import url, include
 from django.urls import path
 
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
-
 from django.contrib import admin
 from users.views import UserViewSet, SendInviteView, UserUpdateView
 
@@ -12,17 +8,16 @@ from rest_framework import routers
 router = routers.DefaultRouter(trailing_slash = False)
 router.register(r'^users', UserViewSet)
 
+from rest_framework.authtoken import views as rviews
+
 urlpatterns = router.urls
 
 urlpatterns += [
-    url(r'^update/user/(?P<pk>\d+)/', UserUpdateView.as_view(), name='user_update'),
-    url(r'send_invite/$', SendInviteView.as_view({"post": "send_the_mail"})),
+    url(r'^update_user/(?P<pk>\d+)/', UserUpdateView.as_view(), name='user_update'),
+    url(r'^send_invite/$', SendInviteView.as_view({"post": "send_the_mail"})),
     # url(r'^user/$', Login.as_view({'get': 'login'}), name='user-login'),
-    # auth-jwt принимает email и password, возвращается token
-    url(r'^auth/jwt/', obtain_jwt_token),
-    url(r'^auth/jwt-refresh/', refresh_jwt_token),
-    # auth-jwt-verify принимает токен и возвращает токен
-    url(r'^auth/jwt-verify/', verify_jwt_token),
+    # принимает email и password, возвращает token
+    url(r'^get_auth_token/$', rviews.obtain_auth_token),
     # Для разработчика
     url(r'^admin/', admin.site.urls),
 ]
