9190cfb8a27a1e0f55f59ab1319f6916e639bcf0
Spanri <kozlova9v@mail.ru>
2019-05-08 17:27:48 +0300

    for heroku

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -131,6 +131,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
