8d63d3123643659c99e25f6a5994e8352308d6c0
Spanri <kozlova9v@mail.ru>
2019-06-01 05:01:53 +0300

    for heroku

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -15,6 +15,25 @@ from corsheaders.defaults import default_methods
 # )
 CORS_ORIGIN_ALLOW_ALL = True
 
+import boto3
+from boto3.s3.transfer import S3Transfer
+
+local_directory = 'media/'
+transfer = S3Transfer(boto3.client('s3', 'your bucket region', 
+                                   aws_access_key_id = '*********',
+                                   aws_secret_access_key='********'))
+client = boto3.client('s3')
+bucket = 'edms-mtuci'
+for root, dirs, files in os.walk(local_directory):
+    for filename in files:
+        local_path = os.path.join(root, filename)
+        relative_path = os.path.relpath(local_path, local_directory)
+        s3_path = os.path.join('your s3 path',relative_path)
+        if filename.endswith('.pdf'):
+            transfer.upload_file(local_path, bucket, s3_path,extra_args={'ACL': 'public-read'})
+        else:
+transfer.upload_file(local_path, bucket, s3_path)
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
