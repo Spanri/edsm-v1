d154fd07cb2ea4e33c17fc4a9528ca008dbf1631
Spanri <kozlova9v@mail.ru>
2019-06-01 06:22:38 +0300

    for heroku

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -20,21 +20,12 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'edms-mtuci'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 #AWS S3 Static
-AWS_STATIC_LOCATION = 'static'
-STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
-# STATICFILES_STORAGE = 'django_auth.storage_backends.StaticStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-#AWS S3 Private Media Upload
-AWS_MEDIA_LOCATION = 'media'
-PRIVATE_FILE_STORAGE = 'django_auth.storage_backends.MediaStorage'
-#AWS S3 Public Media Upload
-AWS_PUBLIC_LOCATION = 'public'
-DEFAULT_FILE_STORAGE = 'django_auth.storage_backends.PublicStorage'
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_files_storage.StaticFilesStorage'
+MEDIAFILES_LOCATION = 'media'
+MEDIAFILES_STORAGE = 'custom_files_storage.MediaFilesStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATIC_URL = 'http://s3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME + '/'
--- docs/models.py
+++ docs/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-from django_auth.storage_backends import MediaStorage
+from django_auth.storage_backends import MediaFilesStorage
 
 class FileCabinet(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
 
 class Doc(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True)
-    file = models.FileField(storage=MediaStorage(), blank=True, null=True)
+    file = models.FileField(storage=MediaFilesStorage(), blank=True, null=True)
     # file = models.FileField(upload_to='', blank=True, null=True)
     size = models.IntegerField(blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True)
