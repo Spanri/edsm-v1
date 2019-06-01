ec69ab14e45b8f013851fdd71f0f69930b88df75
Spanri <kozlova9v@mail.ru>
2019-06-01 23:57:05 +0300

    for heroku

--- forHeroku.bat
+++ forHeroku.bat
@@ -1,7 +1,7 @@
-cd frontend
-npm run build
-cd ..
+REM cd frontend
+REM npm run build
+REM cd ..
 REM c:/Users/Spanri/Desktop/CRM-diplom/venv/Scripts/activate.bat
 REM python manage.py collectstatic
 move %~dp0\frontend\dist\index.html %~dp0\staticfiles\index.html
-move %~dp0\frontend\dist\static %~dp0\staticfiles\static
+move %~dp0\frontend\dist\static %~dp0\staticfiles\
