a5e79821a955ff20596a3f1cce4e61ea6f1c961e
Spanri <kozlova9v@mail.ru>
2019-06-12 02:34:23 +0300

    for heroku

--- aps.py
+++ aps.py
@@ -1,14 +1,43 @@
-from docs.views import job0
-
-REFRESH_INTERVAL = 10
+from users.models import Notif, User
+from apscheduler.schedulers.blocking import BlockingScheduler
+import subprocess
 from apscheduler.schedulers.background import BackgroundScheduler
+REFRESH_INTERVAL = 10
+
 sched = BackgroundScheduler()
 
 # Смотрим, если время подписи истекло, указываем это и
 # посылаем уведомление
 @sched.scheduled_job('interval', seconds=REFRESH_INTERVAL)
 def job():
-    job0()
+    print('sss')
+    # Ищем нотифы, где статус 2 (то есть те, которые надл подписать)
+    notif = Notif.objects.filter(status=2)
+    # Находим людей, которым нужно отправлять уведомление
+    users = User.objects.filter(is_get_notif_expired_email=True)
+    # Проверяем, может в них время date_expire истекло.
+    # Если время истекло, то посылаем уведомления
+    for i, n in enumerate(notif):
+        if n.date_expire and n.date_expire < timezone.now() and n.is_notif_expire == False:
+            if (n.user.profile.first_name != "" and n.user.profile.patronymic != ""):
+                full_name = n.user.profile.second_name + " " + \
+                    n.user.profile.first_name[0] + \
+                    "." + n.user.profile.patronymic[0] + "."
+            else:
+                full_name = n.user.profile.second_name
+            for i, u in enumerate(users):
+                print(u.email)
+                send_mail(
+                    'Уведомление от СЭД МТУСИ',
+                    'Уведомляем, что пользователь ' + full_name + ' (' + n.user.email +
+                    ') просрочил срок подписи документа "' + n.doc.title + '".',
+                    'edmsmtuci@gmail.com',
+                    [u.email, ],
+                    fail_silently=False,
+                )
+            n.is_notif_expire = True
+            n.save()
+
 
 def scheduler_sample():
     print('sdsd')
--- docs/views.py
+++ docs/views.py
@@ -1,6 +1,8 @@
-
+# from apscheduler.schedulers.blocking import BlockingScheduler
+# import subprocess
 from django.core.mail import send_mail
-
+# from apscheduler.schedulers.background import BackgroundScheduler
+# REFRESH_INTERVAL = 10
 import time
 from PIL import Image
 from rest_framework import status
@@ -43,35 +45,6 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.authentication import TokenAuthentication
 from django.utils import timezone
 from datetime import timedelta
-
-def job0():
-    print('sss')
-    # Ищем нотифы, где статус 2 (то есть те, которые надл подписать)
-    notif = Notif.objects.filter(status=2)
-    # Находим людей, которым нужно отправлять уведомление
-    users = User.objects.filter(is_get_notif_expired_email=True)
-    # Проверяем, может в них время date_expire истекло.
-    # Если время истекло, то посылаем уведомления
-    for i, n in enumerate(notif):
-        if n.date_expire and n.date_expire < timezone.now() and n.is_notif_expire == False:
-            if (n.user.profile.first_name != "" and n.user.profile.patronymic != ""):
-                full_name = n.user.profile.second_name + " " + \
-                    n.user.profile.first_name[0] + \
-                    "." + n.user.profile.patronymic[0] + "."
-            else:
-                full_name = n.user.profile.second_name
-            for i, u in enumerate(users):
-                print(u.email)
-                send_mail(
-                    'Уведомление от СЭД МТУСИ',
-                    'Уведомляем, что пользователь ' + full_name + ' (' + n.user.email +
-                    ') просрочил срок подписи документа "' + n.doc.title + '".',
-                    'edmsmtuci@gmail.com',
-                    [u.email, ],
-                    fail_silently=False,
-                )
-            n.is_notif_expire = True
-            n.save()
 
 class DocViewSet(viewsets.ModelViewSet):
     '''
@@ -217,7 +190,46 @@ class AddSignature(generics.ListAPIView):
         serializer = DocSerializer(doc)
         return doc
 
+# sched = BackgroundScheduler()
 
+# # Смотрим, если время подписи истекло, указываем это и
+# # посылаем уведомление
+# @sched.scheduled_job('interval', seconds=REFRESH_INTERVAL)
+# def job():
+#     print('sss')
+#     # Ищем нотифы, где статус 2 (то есть те, которые надл подписать)
+#     notif = Notif.objects.filter(status=2)
+#     # Находим людей, которым нужно отправлять уведомление
+#     users = User.objects.filter(is_get_notif_expired_email=True)
+#     # Проверяем, может в них время date_expire истекло.
+#     # Если время истекло, то посылаем уведомления
+#     for i, n in enumerate(notif):
+#         if n.date_expire and n.date_expire < timezone.now() and n.is_notif_expire == False:
+#             if (n.user.profile.first_name != "" and n.user.profile.patronymic != ""):
+#                 full_name = n.user.profile.second_name + " " + \
+#                 n.user.profile.first_name[0] + \
+#                 "." + n.user.profile.patronymic[0] + "."
+#             else:
+#                 full_name = n.user.profile.second_name
+#             for i, u in enumerate(users):
+#                 print(u.email)
+#                 send_mail(
+#                     'Уведомление от СЭД МТУСИ',
+#                     'Уведомляем, что пользователь ' + full_name + ' (' + n.user.email + 
+#                     ') просрочил срок подписи документа "' + n.doc.title + '".',
+#                     'edmsmtuci@gmail.com',
+#                     [u.email, ],
+#                     fail_silently=False,
+#                 )
+#             n.is_notif_expire = True
+#             n.save()
+
+
+# def scheduler_sample():
+#     print('sdsd')
+#     # subprocess.call('python manage.py scheduler', shell = True, close_fds = True)
+
+# sched.start()
 
 # scheduler = BackgroundScheduler()
 # scheduler.start()
