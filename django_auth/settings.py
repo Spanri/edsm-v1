3dfa6e379e861d9150393d013de9b5e3a23a0286
Spanri <kozlova9v@mail.ru>
2019-06-14 17:23:37 +0300

    for heroku

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -220,7 +220,7 @@ django_heroku.settings(locals())
 # )
 # AWS_DEFAULT_ACL = None
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, 'staticfiles/static/'),
     os.path.join(BASE_DIR, 'frontend/dist/'),
