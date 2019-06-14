32200cdd10368b727f963e03fedb7cf28e17742e
Spanri <kozlova9v@mail.ru>
2019-06-14 03:08:59 +0300

    for heroku

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -82,7 +82,7 @@ ROOT_URLCONF = 'django_auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'staticfiles')],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles/static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
